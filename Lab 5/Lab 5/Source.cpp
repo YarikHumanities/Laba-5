9ad30774e63dd71029ac840c438ad3cc4f36ed63
Yarik <yaroslav.matsapura@gmail.com>
2021-05-14 21:49:29 +0300

    Lenght fixed and insert func tried

--- Lab 5/Lab 5/Source.cpp
+++ Lab 5/Lab 5/Source.cpp
@@ -2,12 +2,16 @@
 #include <string>
 #include <cmath>
 #include <fstream>
+#include <vector>
+#define rad 6367444.65;
 using namespace std;
 
 
 class Location {
 
-private:
+	
+
+public:
 	double latitude;
 	double longitude;
 	string type;
@@ -15,10 +19,10 @@ private:
 	string name;
 	string address;
 
-public:
 	Location(double latitude_, double longitude_, string type_, string subType_, string name_, string address_) : latitude(latitude_),
 		longitude(longitude_), type(type_), subType(subType_), name(name_), address(address_) {
 	}
+
 	void print_loc(/*double latitude, double longitude, string type, string subType, string name, string address*/) {
 		cout << "Latitude: " << this->latitude << " Longitude: " << this->longitude << " Type: " << this->type << " SubType: " << 
 			this->subType << " Name: " << this->name <<
@@ -27,21 +31,37 @@ public:
 
 };
 class Node {
+	
+
+public:
 	Location up_left;
 	Location up_right;
 	Location down_left;
 	Location down_right;
-public:
+
+	vector<Location> locations_v;
+
 	Node(Location up_left, Location up_right, Location down_left, Location down_right) :up_left(up_left), up_right(up_right),
 		down_left(down_left), down_right(down_right) {
 	}
-	double lenght(double longitude1, double latitude1, double longitude2, double latitude2) {
+	double lenght(double& longitude1, double& latitude1, double& longitude2, double& latitude2) {
 		double x1 = (longitude1 * 2 * acos(0.0)) / 180;
 		double y1 = (latitude1 * 2 * acos(0.0)) / 180;
 		double x2 = (longitude2 * 2 * acos(0.0)) / 180;
 		double y2 = (latitude2 * 2 * acos(0.0)) / 180;
 
-		double result = 2 * asin(sqrt(pow(sin((x2 - x1) / 2), 2) + cos(x2) * cos(x1) * pow(sin((y2 - y1) / 2), 2)));
+		double result = 2 * asin(sqrt(pow(sin((x2 - x1) / 2), 2) + cos(x2) * cos(x1) * pow(sin((y2 - y1) / 2), 2)))*rad;
+		return result;
+	}
+
+	void insert(Location loc) {
+		if (!(loc.longitude >= this->up_left.longitude && loc.longitude <= this->down_left.longitude
+			&& loc.latitude >= this->up_left.latitude && loc.latitude <= this->up_right.latitude)) 
+		{
+			//finish up
+		}
+		locations_v.push_back(loc);
+
 	}
 };
 
@@ -98,6 +118,10 @@ public:
 				loc.print_loc();
 			}
 		}
+
+	}
+
+	void insert_loc(Location loc) {
 
 	}
 };
