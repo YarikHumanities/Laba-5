7ff5671e103f4a079b5e1f4445933d329abe9b57
Sergey <sereganiceman@gmail.com>
2021-05-14 23:35:47 +0300

    Form_tree funk added

--- Lab 5/Lab 5/Source.cpp
+++ Lab 5/Lab 5/Source.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #define rad 6367444.65;
 using namespace std;
-
+const int maxleaf = 5;
 double maxFrom(double, double);
 double minFrom(double, double);
 class Location {
@@ -41,10 +41,14 @@ public:
 	Location down_right;
 
 	vector<Location> locations_v;
+	vector<Node> kid;
 
 	Node(Location up_left, Location up_right, Location down_left, Location down_right) :up_left(up_left), up_right(up_right),
 		down_left(down_left), down_right(down_right) {
 	}
+
+	 //theory in ReadMe
+
 	double lenght(double& longitude1, double& latitude1, double& longitude2, double& latitude2) {
 		double x1 = (longitude1 * 2 * acos(0.0)) / 180;
 		double y1 = (latitude1 * 2 * acos(0.0)) / 180;
@@ -55,11 +59,61 @@ public:
 		return result;
 	}
 
+	void form_tree()
+	{
+		if (this->locations_v.size() > maxleaf)
+		{
+			double width = lenght(this->up_left.latitude, this->up_left.longitude, this->up_right.latitude, this->up_right.longitude);
+			double height = lenght(this->up_left.latitude, this->up_left.longitude, this->down_right.latitude, this->down_right.longitude);
+			if (height > width)
+			{
+				//double halfHight = this->up_left.longitude + abs(this->up_left.longitude - this->down_left.longitude) / 2;
+				Location  left(this->up_left.latitude, this->up_left.longitude + abs(this->up_left.longitude - this->down_left.longitude) / 2, this->up_left.type, this->up_left.subType, this->up_left.name, this->up_left.address);
+				Location right(this->up_right.latitude, this->up_left.longitude + abs(this->up_left.longitude - this->down_left.longitude) / 2, this->up_left.type, this->up_left.subType, this->up_left.name, this->up_left.address);
+				Node rectUp(left, this->up_left, right, this->up_right);
+				Node rectD(this->down_left, left, this->down_right, right);
+				while (this->locations_v.size() > 0)
+				{
+					if (rectUp.isinside(this->locations_v[this->locations_v.size() - 1]))
+						rectUp.insert(this->locations_v[this->locations_v.size() - 1]);
+					else
+						rectD.insert(this->locations_v[this->locations_v.size() - 1]);
+					this->locations_v.pop_back();
+				}
+				
+			}
+			else
+			{
+				Location  up(this->up_left.latitude + abs(this->up_left.latitude - this->up_right.latitude) / 2, this->up_left.longitude, this->up_left.type, this->up_left.subType, this->up_left.name, this->up_left.address);
+				Location down(this->up_left.latitude + abs(this->up_left.latitude - this->up_right.latitude) / 2, this->down_left.longitude, this->up_left.type, this->up_left.subType, this->up_left.name, this->up_left.address);
+				Node childL(this->down_left, this->up_left, down, up);
+				Node childR(down, up, this->down_right, this->up_right);
+				while (this->locations_v.size() > 0)
+				{
+					if (childL.isinside(this->locations_v[this->locations_v.size() - 1]))
+						childL.insert(this->locations_v[this->locations_v.size() - 1]);
+					else
+						childR.insert(this->locations_v[this->locations_v.size() - 1]);
+					this->locations_v.pop_back();
+				}
+				this->kid.push_back(childL);
+				this->kid.push_back(childR);
+				childL.form_tree();
+				childR.form_tree();
+			}
+		}
+	}
+	bool isinside(Location loc)
+	{
+		return loc.longitude >= this->up_left.longitude && loc.longitude <= this->down_left.longitude
+			&& loc.latitude >= this->up_left.latitude && loc.latitude <= this->up_right.latitude;
+	}
 	void insert(Location loc) {
 		if (!(loc.longitude >= this->up_left.longitude && loc.longitude <= this->down_left.longitude
 			&& loc.latitude >= this->up_left.latitude && loc.latitude <= this->up_right.latitude)) 
 		{
 			//finish up
+			//changing size of current node if locations does not fits
 			this->up_left.longitude = min(this->up_left.longitude, loc.longitude);
 			this->down_left.longitude = max(this->down_left.longitude, loc.longitude);
 			this->up_right.longitude = min(this->up_left.longitude, loc.longitude);
@@ -69,11 +123,9 @@ public:
 			this->up_left.latitude = min(this->down_left.latitude, loc.latitude);
 			this->down_right.latitude = max(this->down_right.latitude, loc.latitude);
 			this->up_right.latitude = max(this->down_right.latitude, loc.latitude);
-
-
 		}
+		//adding locs to current node
 		locations_v.push_back(loc);
-
 	}
 };
 
@@ -104,6 +156,9 @@ public:
 			string name;
 			string address;
 			while (!inFile.eof()) {
+
+			//changing comas to dots in coordinates
+
 				getline(inFile, latitude, ';');
 				if (latitude == "\n") {
 					break;
@@ -133,7 +188,8 @@ public:
 
 	}
 
-	void insert_loc(Location loc) {
+	void search(double latitude, double longitude, double radius, Node node)
+	{
 
 	}
 };
